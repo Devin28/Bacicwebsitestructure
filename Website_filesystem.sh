0fb8480ec93e0047284b2c261efadd7ee13c07d9
Devin-Brice <devin.brice@hgs.hiddengeniusproject.org>
2018-09-13 17:58:45 -0700

    first commit

---
+++ Website_filesystem.sh
@@ -0,0 +1,24 @@
+pwd									#Printing working directory
+cd ~/Desktop						#Going to the desktop
+mkdir website-filesystem			#Creates website-filesystem folder
+cd website-filesystem				#Goes into website-filesystem
+touch index.html					#Creates index.html file
+mkdir html							#Creates html folder
+mkdir css							#Creates css folder
+mkdir js							#Creates js folder
+mkdir img							#Creates img folder
+mkdir extra							#Creates extra folder
+cd html								#Goes into html
+touch Pg1.html						#Creates Pg1.html file
+touch pg2.html						#Creates Pg2.html file
+cd ../css							#Goes back and moves to css folder
+touch Pg1.css						#Creates Pg1.css
+touch Pg2.css						#Creates Pg2.css
+touch index.css						#Creates index.css file
+cd ../js							#Goes back and moves to js folder
+touch Pg1.js						#Creates Pg1.js
+touch Pg2.js						#Creates Pg2.js
+touch Pg3.js						#Creates Pg3.js
+cd ../extra							#Goes back and moves to extra folder
+mkdir audio							#Creates audio folder
+mkdir video							#Creates video folder
